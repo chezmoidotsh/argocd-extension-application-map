74f41fee1bd5f1cfc1b16986a3818895276b57ac
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2025-07-11 19:12:46 +0200

    :test_tube:: Improve test cases coverage for updateGraph
    updateGraph is a critical function that handle all changes into the
    graph, so it must be properly tested through all test cases.
    
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,9 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
 module.exports = {
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   transform: {
-    "^.+\\.tsx?$": ["ts-jest", {}],
+    '^.+\\.tsx?$': ['ts-jest', {}],
   },
-  collectCoverageFrom: [
-    "src/**/*.{ts,tsx}",
-    "!src/**/*.stories.{ts,tsx}",
-    "!src/**/*.d.ts",
-  ],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.stories.{ts,tsx}', '!src/**/*.d.ts'],
 };
