8a2321c621c5a2ae5955939bf977d05ff8a179a0
Alexandre Nicolaie <xunleii@users.noreply.github.com>
2025-07-22 15:58:48 +0200

    :sparkles:(utils): Add helpers to detect drift between actual sources and default sources
    Users will be able to define default source configuration using
    annotations and, when the source drift, it will be able to easily see if
    there is any drift and revert the drifts.
    For this case, some helpers are required.
    
    Signed-off-by: Alexandre Nicolaie <xunleii@users.noreply.github.com>

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,7 +1,7 @@
 export { ConnectionStatus } from './sse';
-export { isApplication, isApplicationSet, HealthStatus, SyncStatus } from './application';
+export { isApplication, isApplicationSet, HealthStatus, SyncStatus, SourceDriftStatus } from './application';
 export { RankDirection } from './rankdirection';
 export { QuickActionState } from './quickActions';
-export type { Application, ApplicationSet } from './application';
+export type { Application, ApplicationSet, ApplicationSource } from './application';
 export type { RankDirectionType } from './rankdirection';
 export type { SSEEvent, ConnectionStatusDetails } from './sse';
